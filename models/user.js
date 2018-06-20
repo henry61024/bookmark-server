83c13af8cba4d2692011f2d2eb9a87bbf6efe017
henry61024 <henry35208@hotmail.com.tw>
2018-06-21 01:04:53 +0800

    [update] add authId, createTime, role to user scheme

--- models/user.js
+++ models/user.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose'),
       userSchema = new mongoose.Schema({
+          authId: String,
           username: String,
-          description: String
+          description: String,
+          createTime: Date,
+          role: String,
       })
 
 module.exports = mongoose.model('User', userSchema);
